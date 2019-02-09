10b5e1b1cfdf0b009056042faf670fcdc0bf751b
Brian Pate <brianwilliampate@gmail.com>
2019-02-09 14:40:07 -0500

    Final Push

--- Python_Alchemy.py
+++ Python_Alchemy.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 import datetime as dt
+import time
 
 # Database setup
 engine = create_engine("sqlite:///hawaii.sqlite")
@@ -22,6 +23,11 @@ Station = Base.classes.station
 
 # create session from python to the DB
 session = Session(engine)
+# session_2 = Session(engine)
+
+# measurement_results = session.query(Measurement.date, Measurement.prcp, Measurement.tobs).all()
+# station_results = session.query(Station.station, Station.name).all()
+# start_date_results = session.query(Measurement.date).filter(Measurement.date >= 2018-8-10)
 
 # flask setup
 app = Flask(__name__)
@@ -54,23 +60,25 @@ def get_precipitation():
     prcp_data = []
     for data in results:
         prcp_dict = {}
-        prcp_dict[data.date] = data.prcp
+        prcp_dict['Date'] = data.date
+        prcp_dict['Precipitation']:data.prcp
         prcp_data.append(prcp_dict)
-    
+        session.commit()    
     return jsonify(prcp_data)
 
 
 # flask route for list of stations
 @app.route("/api/v1.0/stations")
 def get_stations():
-    results = session.query(Station.station,Station.name).all()
+    results = session.query(Station.station, Station.name).all()
     station_list = []
 
     for data in results:
         station_dict= {}
-        station_dict["Station_ID"]= data.station
-        station_dict["Station_Name"]= data.name
+        station_dict["Station_ID"]= data[0]
+        station_dict["Station_Name"]= data[1]
         station_list.append(station_dict)
+        session.commit()
 
     return jsonify(station_list)
 
@@ -80,6 +88,7 @@ def get_stations():
 @app.route("/api/v1.0/tobs")
 def get_tobs():
     results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-8-23').order_by(Measurement.date).all()
+    session.commit()
     temp_data = []
 
     for data in results:
@@ -87,6 +96,7 @@ def get_tobs():
         temp_dict["Date"]= data[0]
         temp_dict["Temperature"] = data[1]
         temp_data.append(temp_dict)
+        session.commit()
 
     return jsonify(temp_data)
 
@@ -101,11 +111,12 @@ def get_start_date(start):
     temp_data = []
     for data in results:
         temp_dict = {}
-        data["Date"] = float(data[0])
-        data["Average Temperature"] = float(data[1])
-        data["Max Temperature"] = float(data[2])
-        data["Min Temperature"] = float(data[3])
+        temp_dict["Date"] = data[0]
+        temp_dict["Average Temperature"] = float(data[1])
+        temp_dict["Max Temperature"] = float(data[2])
+        temp_dict["Min Temperature"] = float(data[3])
         temp_data.append(temp_dict)
+        session.commit()
 
     return jsonify(temp_data)
 
@@ -118,12 +129,13 @@ def get_date_range(start, end):
     temp_data = []
     for data in results:
         temp_dict = {}
-        data["Start Date"] = start
-        data["End Date"] = end
-        data["Average Temperature"] = float(data[1])
-        data["Max Temperature"] = float(data[2])
-        data["Min Temperature"] = float(data[3])
+        temp_dict["Start Date"] = start
+        temp_dict["End Date"] = end
+        temp_dict["Average Temperature"] = float(data[1])
+        temp_dict["Max Temperature"] = float(data[2])
+        temp_dict["Min Temperature"] = float(data[3])
         temp_data.append(temp_dict)
+        session.commit()
 
     return jsonify(temp_data)
 
